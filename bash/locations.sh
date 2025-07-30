2d5dad15b35e2fab69bb4ce4345ba22acc540778
Patrick Boyle <t.patrick.boyle@pm.me>
2025-07-30 17:09:31 -0500

    init

---
+++ bash/locations.sh
@@ -0,0 +1,18 @@
+
+alias desktop="cd ~/Desktop"
+alias downloads="cd ~/Downloads"
+
+alias projects="cd ~/Projects"
+
+alias notes="projects && cd notes"
+
+alias todos="notes && cd todo"
+alias backlog="todos && vim backlog.txt"
+
+alias dotfiles='cd ~/Projects/dotfiles'
+alias dot='dotfiles'
+
+alias bashrc='dotfiles && vim conf/.bashrc'
+alias vimrc='dotfiles && vim conf/.vimrc'
+alias tmuxconf='dotfiles && vim conf/.tmux.conf'
+
